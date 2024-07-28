29212a0f48130efdf21751877ef927682129fa84
Will <will@Zipcoders-MacBook-Pro-9.local>
2024-07-28 10:58:49 -0400

    made changes

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -4,29 +4,59 @@ import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
 
+
+
     ROT13(Character cs, Character cf) {
+
+
     }
 
     ROT13() {
-    }
 
+    }
 
     public String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder shift = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char shiftedChar = text.charAt(i);
+            if (shiftedChar >= 'a' && shiftedChar <= 'm') {
+                shiftedChar += 13;
+            } else if (shiftedChar >= 'A' && shiftedChar <= 'M') {
+                shiftedChar += 13;
+            } else if (shiftedChar >= 'n' && shiftedChar <= 'z') {
+                shiftedChar -= 13;
+            } else if (shiftedChar >= 'N' && shiftedChar <= 'Z') {
+                shiftedChar -= 13;
 
-        return "";
+            }
+            shift.append(shiftedChar);
+
+
+        }
+        return shift.toString();
     }
-
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
+    StringBuilder rotatedString = new StringBuilder();
 
-        return "";
+    String leftRotate = s.substring(s.indexOf(c));
+    String rightRotate = s.substring(0,s.indexOf(c));
+
+    rotatedString.append(leftRotate);
+    rotatedString.append(rightRotate);
+        return rotatedString.toString();
     }
+
+//    private static String leftRotate(String s, int i) {
+//        String leftRotate = s.substring(i) + s.substring(0, i);
+//        return leftRotate;
+//    }
 
 }
